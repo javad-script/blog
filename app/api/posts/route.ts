25f34943cabc07404df408bf81f46dea47c1dd9e
Djawad <dev.javad.mousavian@gmail.com>
2025-12-26 23:53:10 +0330

    Add search to Get Posts api & fix mock datas

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,11 +1,25 @@
 import { posts } from "@/lib/mockData";
+import { Post } from "@/types/post";
 
-export async function GET() {
+export async function GET(req: Request) {
   try {
-    return Response.json(posts, {
-      headers: { "Content-Type": "application/json" },
-    });
+    const { searchParams } = new URL(req.url);
+    const search = searchParams.get("search")?.trim();
+
+    const filteredPosts = search ? filterPosts(posts, search) : posts;
+
+    return Response.json(filteredPosts);
   } catch {
     return Response.json({ error: "Failed to fetch posts" }, { status: 500 });
   }
+}
+
+function filterPosts(posts: Post[], searchText: string) {
+  const search = searchText.toLowerCase();
+  return posts.filter(
+    (p) =>
+      (p.title?.toLowerCase() ?? "").includes(search) ||
+      (p.description?.toLowerCase() ?? "").includes(search) ||
+      (p.content?.toLowerCase() ?? "").includes(search)
+  );
 }
